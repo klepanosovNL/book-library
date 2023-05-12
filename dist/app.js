3a55338b6ddc933769597b8f381d6cb694d0c063
nikitaklepanosov <nikitaklepanosov@gmil.com>
2023-05-12 12:58:34 +0200

    view pages and routing

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,63 @@
 (function () {
-	'use strict';
+  'use strict';
 
+  class AbstractView {
+    constructor() {
+      this.app = document.getElementById("root");
+    }
 
+    setTitle(title) {
+      document.title = title;
+    }
+
+    render() {
+
+    }
+
+    destroy() {
+
+    }
+  }
+
+  class MainView extends AbstractView {
+    constructor() {
+      super();
+
+      this.setTitle("Book search");
+    }
+
+    render() {
+      const main = document.createElement("div");
+      main.innerHTML = "test";
+      this.app.innerHTML = "";
+      this.app.append(main);
+    }
+  }
+
+  class App {
+    routes = [
+      {
+        path: "",
+        view: MainView,
+      },
+    ];
+
+    constructor() {
+      window.addEventListener("hashchange", this.route.bind(this));
+      this.route();
+    }
+
+    route() {
+      if (this.currentView) {
+        this.currentView.destroy();
+      }
+
+      const View = this.routes.find((r) => r.path === location.hash).view;
+      this.currentView = new View();
+      this.currentView.render();
+    }
+  }
+
+  new App();
 
 })();
--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-"use strict";
+import { MainView } from "./views/main/main.js";
+
+class App {
+  routes = [
+    {
+      path: "",
+      view: MainView,
+    },
+  ];
+
+  constructor() {
+    window.addEventListener("hashchange", this.route.bind(this));
+    this.route();
+  }
+
+  route() {
+    if (this.currentView) {
+      this.currentView.destroy();
+    }
+
+    const View = this.routes.find((r) => r.path === location.hash).view;
+    this.currentView = new View();
+    this.currentView.render();
+  }
+}
+
+new App();
